4fd56bc78116251ec3456d7967e05bdb2a8dcdab
Shahab Jahangiri <shahab.j3@gmail.com>
2020-05-17 14:04:10 +1000

    created the routes

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,7 +2,7 @@
 
 class Cocktail < ApplicationRecord
     has_many :doses, dependent: :destroy
-    has_many :ingredient, through: :doses
+    has_many :ingredients, through: :doses
     has_many :reviews, dependent: :destroy
     validates :name, presence: true, uniqueness: true
     mount_uploader :photo, PhotoUploader
