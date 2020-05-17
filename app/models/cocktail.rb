c6164297c9cc3bf283a6edf91794b83b5f225a6e
Shahab Jahangiri <shahab.j3@gmail.com>
2020-05-17 13:08:41 +1000

    added has many and blongs to for our model

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class Cocktail < ApplicationRecord
+    has_many :doses, dependent: :destroy
+    has_many :ingredient, through: :doses
+    has_many :reviews, dependent: :destroy
+    validates :name, presence: true, uniqueness: true
+    mount_uploader :photo, PhotoUploader
 end
